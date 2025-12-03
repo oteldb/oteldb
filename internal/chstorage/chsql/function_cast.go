27a1dc7e43353b7fe4f24f790520ebb1a4fe8957
tdakkota <tdakkota@yandex.ru>
2025-12-03 09:13:13 +0300

    feat(chsql): add `toUInt64` and `toInt64`

--- internal/chstorage/chsql/function_cast.go
+++ internal/chstorage/chsql/function_cast.go
@@ -10,6 +10,16 @@ func ToString(arg Expr) Expr {
 	return Function("toString", arg)
 }
 
+// ToInt64 returns `toInt64(<arg>)` function call expression.
+func ToInt64(arg Expr) Expr {
+	return Function("toInt64", arg)
+}
+
+// ToUInt64 returns `toUInt64(<arg>)` function call expression.
+func ToUInt64(arg Expr) Expr {
+	return Function("toUInt64", arg)
+}
+
 // ToFloat64 returns `toFloat64(<arg>)` function call expression.
 func ToFloat64(arg Expr) Expr {
 	return Function("toFloat64", arg)
