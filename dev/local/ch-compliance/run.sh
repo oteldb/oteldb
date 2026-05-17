7855825f279e21143eb3c7d283e9caef970ae1b2
tdakkota <github@tdakkota.dev>
2026-05-17 12:43:53 +0300

    ci(ch-compliance): add query repeats flag to compliance tester

--- dev/local/ch-compliance/run.sh
+++ dev/local/ch-compliance/run.sh
@@ -19,7 +19,9 @@ RANGE="1m"
 END="1m"
 go run github.com/go-faster/oteldb/cmd/promql-compliance-tester \
   -end "${END}" -range "${RANGE}" \
-  -config-file promql-test-queries.yml -config-file test-oteldb.yml -output-format json > result.oteldb.json || true
+  -query-repeats=3 \
+  -config-file promql-test-queries.yml -config-file test-oteldb.yml \
+  -output-format json > result.oteldb.json || true
 
 go run ./cmd/compliance-verify result.oteldb.json
 
