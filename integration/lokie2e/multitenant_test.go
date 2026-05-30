4ff14b218331135da81b6cc80990816f031215e8
tdakkota <github@tdakkota.dev>
2026-05-30 18:10:47 +0300

    test(integration): enhance multitenancy tests for inserter routing and multi-tenant queries

--- integration/lokie2e/multitenant_test.go
+++ integration/lokie2e/multitenant_test.go
@@ -1,20 +1,25 @@
 package lokie2e_test
 
 import (
+	"context"
 	"net/http"
+	"net/http/httptest"
+	"slices"
 	"strings"
 	"testing"
 
 	"github.com/go-faster/sdk/zctx"
 	"github.com/google/uuid"
 	"github.com/stretchr/testify/require"
-
-	"net/http/httptest"
+	"go.opentelemetry.io/collector/pdata/pcommon"
+	"golang.org/x/exp/maps"
 
 	"github.com/go-faster/oteldb/integration"
+	"github.com/go-faster/oteldb/integration/lokie2e"
 	"github.com/go-faster/oteldb/internal/chstorage"
 	"github.com/go-faster/oteldb/internal/logql"
 	"github.com/go-faster/oteldb/internal/logql/logqlengine"
+	"github.com/go-faster/oteldb/internal/logstorage"
 	"github.com/go-faster/oteldb/internal/lokiapi"
 	"github.com/go-faster/oteldb/internal/lokihandler"
 	"github.com/go-faster/oteldb/internal/multitenancy"
@@ -48,7 +53,10 @@ func TestMultitenancy(t *testing.T) {
 	})
 
 	mapper := multitenancy.NewTenantMapper(multitenancy.TenantMapperConfig{
-		DefaultTenant: "100",
+		Tenants: []multitenancy.TenantRule{
+			{ID: "tenant2", KeyAttributes: map[string]string{"tenant": "tenant2"}},
+		},
+		DefaultTenant: "tenant1",
 	})
 	inserter, err := chstorage.NewInserter(c, chstorage.InserterOptions{
 		Tables:         tables,
@@ -58,6 +66,17 @@ func TestMultitenancy(t *testing.T) {
 	require.NoError(t, err)
 
 	set := loadTestData(ctx, t, inserter)
+
+	// Consume again as tenant2.
+	consumer, err := logstorage.NewConsumer(inserter, logstorage.ConsumerOptions{})
+	require.NoError(t, err)
+	for _, b := range set.Batches {
+		resLogs := b.ResourceLogs()
+		for j := 0; j < resLogs.Len(); j++ {
+			resLogs.At(j).Resource().Attributes().PutStr("tenant", "tenant2")
+		}
+		require.NoError(t, consumer.ConsumeLogs(ctx, b))
+	}
 
 	querier, err := chstorage.NewQuerier(c, chstorage.QuerierOptions{
 		Tables:         tables,
@@ -85,8 +104,11 @@ func TestMultitenancy(t *testing.T) {
 
 	resolver := static.NewResolver(static.Config{
 		ReadDecisions: map[string]multitenancy.Decision{
-			"100": {Enabled: true, TenantIDs: []string{"100"}},
-			"200": {Enabled: true, TenantIDs: []string{"200"}},
+			"tenant1":           {Enabled: true, TenantIDs: []string{"tenant1"}},
+			"tenant2":           {Enabled: true, TenantIDs: []string{"tenant2"}},
+			"tenant_none":       {Enabled: true, TenantIDs: []string{"tenant_none"}},
+			"tenant1_2":         {Enabled: true, TenantIDs: []string{"tenant1", "tenant2"}},
+			"tenant_superadmin": {Enabled: false},
 		},
 	})
 	mw := multitenancy.NewMiddleware(multitenancy.MiddlewareConfig{
@@ -96,45 +118,164 @@ func TestMultitenancy(t *testing.T) {
 	s := httptest.NewServer(mw(lokih))
 	t.Cleanup(s.Close)
 
-	c100, err := lokiapi.NewClient(s.URL,
-		lokiapi.WithClient(&http.Client{
-			Transport: &authRoundTripper{next: http.DefaultTransport, token: "100"},
-		}),
+	c1, err := lokiapi.NewClient(s.URL,
+		lokiapi.WithClient(&http.Client{Transport: &authRoundTripper{next: http.DefaultTransport, token: "tenant1"}}),
 		lokiapi.WithTracerProvider(provider),
 	)
 	require.NoError(t, err)
 
-	c200, err := lokiapi.NewClient(s.URL,
-		lokiapi.WithClient(&http.Client{
-			Transport: &authRoundTripper{next: http.DefaultTransport, token: "200"},
-		}),
+	c2, err := lokiapi.NewClient(s.URL,
+		lokiapi.WithClient(&http.Client{Transport: &authRoundTripper{next: http.DefaultTransport, token: "tenant2"}}),
 		lokiapi.WithTracerProvider(provider),
 	)
 	require.NoError(t, err)
 
-	t.Run("Tenant100", func(t *testing.T) {
-		r, err := c100.QueryRange(ctx, lokiapi.QueryRangeParams{
-			Query: `{service_name=~".+"}`,
-			Start: lokiapi.NewOptLokiTime(asLokiTime(set.Start)),
-			End:   lokiapi.NewOptLokiTime(asLokiTime(set.End)),
-		})
-		require.NoError(t, err)
-		
-		streams, ok := r.Data.GetStreamsResult()
-		require.True(t, ok)
-		require.NotEmpty(t, streams.Result)
+	cNone, err := lokiapi.NewClient(s.URL,
+		lokiapi.WithClient(&http.Client{Transport: &authRoundTripper{next: http.DefaultTransport, token: "tenant_none"}}),
+		lokiapi.WithTracerProvider(provider),
+	)
+	require.NoError(t, err)
+
+	c1_2, err := lokiapi.NewClient(s.URL,
+		lokiapi.WithClient(&http.Client{Transport: &authRoundTripper{next: http.DefaultTransport, token: "tenant1_2"}}),
+		lokiapi.WithTracerProvider(provider),
+	)
+	require.NoError(t, err)
+
+	cSuperadmin, err := lokiapi.NewClient(s.URL,
+		lokiapi.WithClient(&http.Client{Transport: &authRoundTripper{next: http.DefaultTransport, token: "tenant_superadmin"}}),
+		lokiapi.WithTracerProvider(provider),
+	)
+	require.NoError(t, err)
+
+	t.Run("Tenant1", func(t *testing.T) {
+		checkLokiTenant(ctx, t, c1, set, true, false)
 	})
 
-	t.Run("Tenant200", func(t *testing.T) {
-		r, err := c200.QueryRange(ctx, lokiapi.QueryRangeParams{
-			Query: `{service_name=~".+"}`,
+	t.Run("Tenant2", func(t *testing.T) {
+		checkLokiTenant(ctx, t, c2, set, false, true)
+	})
+
+	t.Run("TenantNone", func(t *testing.T) {
+		checkLokiTenant(ctx, t, cNone, set, false, false)
+	})
+
+	t.Run("Tenant1_2", func(t *testing.T) {
+		checkLokiTenant(ctx, t, c1_2, set, true, true)
+	})
+
+	t.Run("TenantSuperadmin", func(t *testing.T) {
+		checkLokiTenant(ctx, t, cSuperadmin, set, true, true)
+	})
+}
+
+func checkLokiTenant(
+	ctx context.Context,
+	t *testing.T,
+	c *lokiapi.Client,
+	set *lokie2e.BatchSet,
+	hasTenant1, hasTenant2 bool,
+) {
+	// 1. Check Labels
+	r, err := c.Labels(ctx, lokiapi.LabelsParams{
+		Start: lokiapi.NewOptLokiTime(asLokiTime(set.Start)),
+		End:   lokiapi.NewOptLokiTime(asLokiTime(set.End)),
+	})
+	require.NoError(t, err)
+
+	var expectedLabels []string
+	if !hasTenant1 && !hasTenant2 {
+		expectedLabels = []string{
+			"detected_level", "level", "msg", "service_instance_id",
+			"service_name", "service_namespace", "span_id", "trace_id",
+		}
+	} else {
+		expectedLabels = maps.Keys(set.Labels)
+		if hasTenant2 {
+			expectedLabels = append(expectedLabels, "tenant")
+		}
+	}
+	slices.Sort(expectedLabels)
+	gotLabels := r.Data
+	slices.Sort(gotLabels)
+	require.Equal(t, expectedLabels, gotLabels)
+
+	// 2. Check Label Values
+	for _, labelName := range gotLabels {
+		valRes, err := c.LabelValues(ctx, lokiapi.LabelValuesParams{
+			Name:  labelName,
 			Start: lokiapi.NewOptLokiTime(asLokiTime(set.Start)),
 			End:   lokiapi.NewOptLokiTime(asLokiTime(set.End)),
 		})
 		require.NoError(t, err)
 
-		streams, ok := r.Data.GetStreamsResult()
-		require.True(t, ok)
+		var expectedValues []string
+		switch {
+		case !hasTenant1 && !hasTenant2:
+			if labelName == "level" || labelName == "detected_level" {
+				unique := map[string]struct{}{}
+				for _, label := range set.Labels[labelName] {
+					unique[label.Value] = struct{}{}
+				}
+				expectedValues = maps.Keys(unique)
+			} else {
+				expectedValues = []string{}
+			}
+		case labelName == "tenant":
+			expectedValues = []string{"tenant2"}
+		default:
+			unique := map[string]struct{}{}
+			for _, label := range set.Labels[labelName] {
+				unique[label.Value] = struct{}{}
+			}
+			expectedValues = maps.Keys(unique)
+		}
+		slices.Sort(expectedValues)
+		gotValues := valRes.Data
+		slices.Sort(gotValues)
+		require.Equal(t, expectedValues, gotValues, "values for label %q", labelName)
+	}
+
+	// 3. Check data (QueryRange)
+	qRangeRes, err := c.QueryRange(ctx, lokiapi.QueryRangeParams{
+		Query: `{service_name=~".+"}`,
+		Start: lokiapi.NewOptLokiTime(asLokiTime(set.Start)),
+		End:   lokiapi.NewOptLokiTime(asLokiTime(set.End)),
+		Limit: lokiapi.NewOptInt(1000),
+	})
+	require.NoError(t, err)
+
+	streams, ok := qRangeRes.Data.GetStreamsResult()
+	require.True(t, ok)
+
+	var expectedCount int
+	if hasTenant1 {
+		expectedCount += len(set.Records)
+	}
+	if hasTenant2 {
+		expectedCount += len(set.Records)
+	}
+
+	if expectedCount == 0 {
 		require.Empty(t, streams.Result)
-	})
+	} else {
+		actualEntries := 0
+		for _, stream := range streams.Result {
+			for _, entry := range stream.Values {
+				actualEntries++
+				// Check that the record exists and matches.
+				record, ok := set.Records[pcommon.Timestamp(entry.T)]
+				require.Truef(t, ok, "can't find log record %d", entry.T)
+				require.Equal(t, record.Body().AsString(), entry.V)
+				labelSetHasAttrs(t, stream.Stream.Value, record.Attributes())
+
+				tenantVal, ok := stream.Stream.Value["tenant"]
+				if ok {
+					require.True(t, hasTenant2)
+					require.Equal(t, "tenant2", tenantVal)
+				}
+			}
+		}
+		require.Equal(t, expectedCount, actualEntries)
+	}
 }
--- integration/tempoe2e/multitenant_test.go
+++ integration/tempoe2e/multitenant_test.go
@@ -1,21 +1,30 @@
 package tempoe2e_test
 
 import (
+	"context"
+	"io"
 	"net/http"
 	"net/http/httptest"
+	"slices"
 	"strings"
 	"testing"
 
 	"github.com/google/uuid"
 	"github.com/stretchr/testify/require"
+	"go.opentelemetry.io/collector/pdata/pcommon"
+	"go.opentelemetry.io/collector/pdata/ptrace"
+	"golang.org/x/exp/maps"
 
 	"github.com/go-faster/oteldb/integration"
+	"github.com/go-faster/oteldb/integration/tempoe2e"
 	"github.com/go-faster/oteldb/internal/chstorage"
 	"github.com/go-faster/oteldb/internal/multitenancy"
 	"github.com/go-faster/oteldb/internal/multitenancy/static"
+	"github.com/go-faster/oteldb/internal/otelstorage"
 	"github.com/go-faster/oteldb/internal/tempoapi"
 	"github.com/go-faster/oteldb/internal/tempohandler"
 	"github.com/go-faster/oteldb/internal/traceql/traceqlengine"
+	"github.com/go-faster/oteldb/internal/tracestorage"
 )
 
 type authRoundTripper struct {
@@ -45,7 +54,10 @@ func TestMultitenancy(t *testing.T) {
 	})
 
 	mapper := multitenancy.NewTenantMapper(multitenancy.TenantMapperConfig{
-		DefaultTenant: "100",
+		Tenants: []multitenancy.TenantRule{
+			{ID: "tenant2", KeyAttributes: map[string]string{"tenant": "tenant2"}},
+		},
+		DefaultTenant: "tenant1",
 	})
 	inserter, err := chstorage.NewInserter(c, chstorage.InserterOptions{
 		Tables:         tables,
@@ -55,6 +67,16 @@ func TestMultitenancy(t *testing.T) {
 	require.NoError(t, err)
 
 	set := loadTestData(ctx, t, inserter)
+
+	// Consume again as tenant2.
+	consumer := tracestorage.NewConsumer(inserter)
+	for _, b := range set.Batches {
+		resSpans := b.ResourceSpans()
+		for j := 0; j < resSpans.Len(); j++ {
+			resSpans.At(j).Resource().Attributes().PutStr("tenant", "tenant2")
+		}
+		require.NoError(t, consumer.ConsumeTraces(ctx, b))
+	}
 
 	querier, err := chstorage.NewQuerier(c, chstorage.QuerierOptions{
 		Tables:         tables,
@@ -75,8 +97,11 @@ func TestMultitenancy(t *testing.T) {
 
 	resolver := static.NewResolver(static.Config{
 		ReadDecisions: map[string]multitenancy.Decision{
-			"100": {Enabled: true, TenantIDs: []string{"100"}},
-			"200": {Enabled: true, TenantIDs: []string{"200"}},
+			"tenant1":           {Enabled: true, TenantIDs: []string{"tenant1"}},
+			"tenant2":           {Enabled: true, TenantIDs: []string{"tenant2"}},
+			"tenant_none":       {Enabled: true, TenantIDs: []string{"tenant_none"}},
+			"tenant1_2":         {Enabled: true, TenantIDs: []string{"tenant1", "tenant2"}},
+			"tenant_superadmin": {Enabled: false},
 		},
 	})
 	mw := multitenancy.NewMiddleware(multitenancy.MiddlewareConfig{
@@ -86,33 +111,164 @@ func TestMultitenancy(t *testing.T) {
 	s := httptest.NewServer(mw(tempoh))
 	t.Cleanup(s.Close)
 
-	c100, err := tempoapi.NewClient(s.URL,
-		tempoapi.WithClient(&http.Client{
-			Transport: &authRoundTripper{next: http.DefaultTransport, token: "100"},
-		}),
+	c1, err := tempoapi.NewClient(s.URL,
+		tempoapi.WithClient(&http.Client{Transport: &authRoundTripper{next: http.DefaultTransport, token: "tenant1"}}),
 		tempoapi.WithTracerProvider(provider),
 	)
 	require.NoError(t, err)
 
-	c200, err := tempoapi.NewClient(s.URL,
-		tempoapi.WithClient(&http.Client{
-			Transport: &authRoundTripper{next: http.DefaultTransport, token: "200"},
-		}),
+	c2, err := tempoapi.NewClient(s.URL,
+		tempoapi.WithClient(&http.Client{Transport: &authRoundTripper{next: http.DefaultTransport, token: "tenant2"}}),
 		tempoapi.WithTracerProvider(provider),
 	)
 	require.NoError(t, err)
 
-	t.Run("Tenant100", func(t *testing.T) {
-		r, err := c100.SearchTags(ctx, tempoapi.SearchTagsParams{})
-		require.NoError(t, err)
-		require.NotEmpty(t, r.TagNames)
+	cNone, err := tempoapi.NewClient(s.URL,
+		tempoapi.WithClient(&http.Client{Transport: &authRoundTripper{next: http.DefaultTransport, token: "tenant_none"}}),
+		tempoapi.WithTracerProvider(provider),
+	)
+	require.NoError(t, err)
+
+	cBoth, err := tempoapi.NewClient(s.URL,
+		tempoapi.WithClient(&http.Client{Transport: &authRoundTripper{next: http.DefaultTransport, token: "tenant1_2"}}),
+		tempoapi.WithTracerProvider(provider),
+	)
+	require.NoError(t, err)
+
+	cSuperadmin, err := tempoapi.NewClient(s.URL,
+		tempoapi.WithClient(&http.Client{Transport: &authRoundTripper{next: http.DefaultTransport, token: "tenant_superadmin"}}),
+		tempoapi.WithTracerProvider(provider),
+	)
+	require.NoError(t, err)
+
+	t.Run("Tenant1", func(t *testing.T) {
+		checkTempoTenant(ctx, t, c1, set, true, false)
 	})
 
-	t.Run("Tenant200", func(t *testing.T) {
-		r, err := c200.SearchTags(ctx, tempoapi.SearchTagsParams{})
-		require.NoError(t, err)
+	t.Run("Tenant2", func(t *testing.T) {
+		checkTempoTenant(ctx, t, c2, set, false, true)
+	})
+
+	t.Run("TenantNone", func(t *testing.T) {
+		checkTempoTenant(ctx, t, cNone, set, false, false)
+	})
+
+	t.Run("TenantBoth", func(t *testing.T) {
+		checkTempoTenant(ctx, t, cBoth, set, true, true)
+	})
+
+	t.Run("TenantSuperadmin", func(t *testing.T) {
+		checkTempoTenant(ctx, t, cSuperadmin, set, true, true)
+	})
+}
+
+func checkTempoTenant(
+	ctx context.Context,
+	t *testing.T,
+	c *tempoapi.Client,
+	set tempoe2e.BatchSet,
+	hasTenant1, hasTenant2 bool,
+) {
+	// 1. SearchTags.
+	r, err := c.SearchTags(ctx, tempoapi.SearchTagsParams{
+		Start: tempoTime(set.Start.AsTime()),
+		End:   tempoTime(set.End.AsTime()),
+	})
+	require.NoError(t, err)
+
+	if !hasTenant1 && !hasTenant2 {
 		require.Empty(t, r.TagNames)
+	} else {
+		expectedTags := maps.Keys(set.Tags)
+		if hasTenant2 {
+			expectedTags = append(expectedTags, "tenant")
+		}
+		slices.Sort(expectedTags)
+		gotTags := r.TagNames
+		slices.Sort(gotTags)
+		require.Equal(t, expectedTags, gotTags)
+
+		// 2. SearchTagValues.
+		for _, tagName := range gotTags {
+			valRes, err := c.SearchTagValues(ctx, tempoapi.SearchTagValuesParams{
+				TagName: tagName,
+				Start:   tempoTime(set.Start.AsTime()),
+				End:     tempoTime(set.End.AsTime()),
+			})
+			require.NoError(t, err)
+
+			var expectedValues []string
+			if tagName == "tenant" {
+				expectedValues = []string{"tenant2"}
+			} else {
+				tags := set.Tags[tagName]
+				valMap := make(map[string]struct{})
+				for _, tag := range tags {
+					valMap[tag.Value] = struct{}{}
+				}
+				expectedValues = maps.Keys(valMap)
+			}
+			slices.Sort(expectedValues)
+			gotValues := valRes.TagValues
+			slices.Sort(gotValues)
+			require.Equal(t, expectedValues, gotValues, "values for tag %q", tagName)
+		}
+	}
+
+	// 3. Search.
+	searchRes, err := c.Search(ctx, tempoapi.SearchParams{
+		Q:     tempoapi.NewOptString(`{}`),
+		Start: tempoTime(set.Start.AsTime()),
+		End:   tempoTime(set.End.AsTime()),
+		Limit: tempoapi.NewOptInt(100),
 	})
-	
-	_ = set
+	require.NoError(t, err)
+
+	var expectedTracesCount int
+	if hasTenant1 || hasTenant2 {
+		expectedTracesCount = len(set.Traces)
+	}
+
+	if !hasTenant1 && !hasTenant2 {
+		require.Empty(t, searchRes.Traces)
+	} else {
+		require.Len(t, searchRes.Traces, expectedTracesCount)
+		for _, traceMetadata := range searchRes.Traces {
+			traceID := pcommon.TraceID(uuid.MustParse(traceMetadata.TraceID))
+			_, exists := set.Traces[traceID]
+			require.True(t, exists, "trace %s must exist in test data", traceMetadata.TraceID)
+		}
+	}
+
+	// 4. TraceByID.
+	for traceID, expectedTrace := range set.Traces {
+		tid := otelstorage.TraceID(traceID).Hex()
+		r, err := c.TraceByID(ctx, tempoapi.TraceByIDParams{
+			TraceID: tid,
+			Start:   tempoTime(set.Start.AsTime()),
+			End:     tempoTime(set.End.AsTime()),
+		})
+		require.NoError(t, err)
+
+		if !hasTenant1 && !hasTenant2 {
+			require.IsType(t, &tempoapi.TraceByIDNotFound{}, r)
+		} else {
+			require.IsType(t, &tempoapi.TraceByID{}, r)
+			data, err := io.ReadAll(r.(*tempoapi.TraceByID))
+			require.NoError(t, err)
+
+			var u ptrace.ProtoUnmarshaler
+			resp, err := u.UnmarshalTraces(data)
+			require.NoError(t, err)
+
+			var expectedSpansCount int
+			if hasTenant1 {
+				expectedSpansCount += len(expectedTrace.Spanset)
+			}
+			if hasTenant2 {
+				expectedSpansCount += len(expectedTrace.Spanset)
+			}
+			require.Equal(t, expectedSpansCount, resp.SpanCount(), "span count for trace %s", tid)
+		}
+	}
 }
